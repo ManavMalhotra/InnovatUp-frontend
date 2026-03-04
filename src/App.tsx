b6f97f718ee5af4d340827b30958f1a5ae296909
Manav Malhotra <64147782+ManavMalhotra@users.noreply.github.com>
2026-03-05 00:14:27 +0530

    fixed minor bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useEffect, lazy, Suspense } from "react";
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import {
   BrowserRouter as Router,
   Routes,
